91dde57bee62a5eea8aefce85ef80bdfbcb8e72b
petrl691 <petrl691@newschool.edu>
2015-10-16 16:12:20 +0000

    Adding index file

---
+++ scripts.js
@@ -0,0 +1,10 @@
+console.log("hello");
+
+var symbols= ["()", "*",")("];
+
+var pattern= ["()","*",")("];
+
+for (var increment=100; increment>symbols.length; increment--)
+
+console.log(pattern);
+
